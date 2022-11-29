c6461e65f0798de6282e422aa858c4bcab5817ea
rehanullah748 <rehanullah748@gmail.com>
2022-11-29 17:50:45 +0500

    changes made

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,8 @@
 const Product = ({product}) => {
     const {title,image,price} = product;
   return (
-    <div className="w-full sm:w-6/12 md:w-4/12 lg:w-3/12 p-5">
-        <div className="w-full h-[200px] overflow-hidden ">
+    <div className="w-full sm:w-6/12 md:w-4/12 lg:w-3/12 p-5 border border-solid border-red-600 m-2">
+        <div className="w-full h-[200px] overflow-hidden border border-indigo-600">
         <img src={image} alt={title} className="w-full h-full object-cover rounded-tl-lg rounded-tr-lg hover:scale-[1.2] transition-all duration-700" />
         </div>
         <p className="mt-2 text-lg">{title.slice(0,50)}...</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,12 @@ import Product from "../components/Product";
 import { ShopContext } from "../store/ShopProvider";
 const Home = () => {
     const {state: {products,loader}} = useContext(ShopContext);
-    // const {products, loader} = state;
+    // const {products, loader} = state;   question: are we can use this instead of above
   return (
     <div>
       <Header />
-      <div className="max-w-screen-xl w-full my-10 mx-auto">
-      {loader ? <Loader /> : <div className="flex flex-wrap">
+      <div className="max-w-screen-xl w-full my-10 mx-auto border border-dashed border-black">
+      {loader ? <Loader /> : <div className="flex flex-wrap border border-dotted border-blue-700 m-2">
         {products.map( product => (
         <Product key={product.id} product={product} />
       ))}
